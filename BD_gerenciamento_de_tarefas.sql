edf07ae82447726cd0f13767b2e33ff19508a234
IcaroSantos21 <icaro.santosdev@gmail.com>
2025-10-22 08:55:32 -0300

    Criação do banco de dados

--- BD_gerenciamento_de_tarefas.sql
+++ BD_gerenciamento_de_tarefas.sql
@@ -1,7 +1,7 @@
 /* CREATE DATABASE gerenciador_de_tarefas; */
--- USE gerenciador_de_tarefas;
--- CREATE TABLE tarefas(
--- id_tarefas INT AUTO_INCREMENT PRIMARY KEY,
--- descricao VARCHAR(255) NOT NULL,
--- data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
--- status VARCHAR(50) DEFAULT 'pendente')
+USE gerenciador_de_tarefas;
+CREATE TABLE tarefas(
+id_tarefas INT AUTO_INCREMENT PRIMARY KEY,
+descricao VARCHAR(255) NOT NULL,
+data_criacao DATETIME DEFAULT CURRENT_TIMESTAMP,
+status VARCHAR(50) DEFAULT 'pendente')
