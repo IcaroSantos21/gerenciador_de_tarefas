8bcf1b026722fea7e8fb514d4c59c64f0ce9e6c0
IcaroSantos21 <icaro.santosdev@gmail.com>
2025-10-22 07:39:39 -0300

    Criação das funções

--- main.py
+++ main.py
@@ -13,3 +13,22 @@ def conector():
         return mydb
     except mysql.connector.Error as err:
         print(f'Erro ao se conectar ao MySQL {err}')
+        return None
+
+def adicionar_tarefas():
+    ...
+
+def lista_tarefas():
+    ...
+
+def concluir_tarefas():
+    ...
+
+def remover_tarefas():
+    ...
+
+def mostrar_menu():
+    ...
+
+def main():
+    ...
