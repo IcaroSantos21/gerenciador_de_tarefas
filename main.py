9f21b62fb7fbc33a0251d424a6b5f5e921f9bb8a
IcaroSantos21 <icaro.santosdev@gmail.com>
2025-10-22 07:47:10 -0300

    Criação da função para mostrar o menu

--- main.py
+++ main.py
@@ -15,6 +15,19 @@ def conector():
         print(f'Erro ao se conectar ao MySQL {err}')
         return None
 
+def mostrar_menu():
+    print('-------------------------------------')
+    print('SISTEMA DE GERENCIAMENTO DE TAREFAS')
+    print('-------------------------------------')
+    print('1 - Adicionar Tarefa')
+    print('2 - Listar Tarefa')
+    print('3 - Concluir Tarefa (Por ID)')
+    print('4 - Remover Tarefa (Por ID)')
+    print('5 - Sair')
+    print()
+    return input('Selecione uma opção: ')
+
+
 def adicionar_tarefas():
     ...
 
@@ -25,9 +38,6 @@ def concluir_tarefas():
     ...
 
 def remover_tarefas():
-    ...
-
-def mostrar_menu():
     ...
 
 def main():
